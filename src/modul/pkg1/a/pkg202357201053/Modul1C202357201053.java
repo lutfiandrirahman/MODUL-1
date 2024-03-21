b83b7abad45ba141a794c9c80cc28b141e1ce283
lutfiandrirahman <163151188+lutfiandrirahman@users.noreply.github.com>
2024-03-21 18:45:04 +0700

    commit module 1 C

---
+++ src/modul/pkg1/a/pkg202357201053/Modul1C202357201053.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul.pkg1.c.pkg202357201053;
+
+public class Modul1C202357201053 {
+public static void main(String[] args) {
+
+String[][] data = {{"ABDUL", "\t085646668991", "Kediri"},
+    {"KUSNO", "085646668992", "Trenggalek"},
+    {"PONIRAN","085646668999","Bojonegoro"}};
+
+System.out.println("NAMA\t\tALAMAT\t\t\tTELEPON");
+for (String[] ke : data) {
+System.out.println(ke[0] + "\t\t" + ke [2] + "\t\t" + ke [1]);
+
+    } 
+  } 
+}
+   
+    
